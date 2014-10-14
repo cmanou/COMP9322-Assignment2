c1a6de4582d2cacd1843f09584796a960a2da01f
Christopher Manouvrier <cmanou@mac.com>
2014-10-14 23:13:05 +1100

    More scaffolding for barista app think i have all the actions

---
+++ cs9322.coffee.barista/src/cs9322/coffee/barista/actions/OrderPrepare.java
@@ -0,0 +1,15 @@
+package cs9322.coffee.barista.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OrderPrepare implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
